141ec6abc3afc0a5950763128874c670d76848fd
Omar Reyes L. <relo2210@gmail.com>
2025-10-25 02:05:07 -0600

    feat: Create initial game board and entities
    Initializes all the core components required to start a game session.
    
    - Sets up the main screen with specific dimensions and background color.
    - Creates and styles the snake, apple, and scoreboard turtle objects.
    - Defines initial game state variables like delay and game status.

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 import turtle
 import time
 import random
+
+class Snake:
+    """
+    Initialize the Snake Game modules.
+
+    Attributes:
+        screen (turtle.Screen): The main game window.
+        snake (turtle.Turtle): The turtle object for the snake's head.
+        sdata (turtle.Turtle): The turtle object for the scoreboard.
+        apples (turtle.Turtle): The turtle object for the food.
+        delay (float): The refresh delay for the game loop.
+        end_game (bool): Flag to indicate if the game is over.
+    """
+    def __init__(self):
+        """Initializes all the core components required to start a game session."""
+        self.screen = turtle.Screen()
+        self.snake = turtle.Turtle()
+        self.sdata = turtle.Turtle()
+        self.apples = turtle.Turtle()
+        self.delay = 0.1
+        self.end_game = False
+
+        self.screen.setup(width=800, height=800); self.screen.title("Snake Game"); self.screen.bgcolor("#329F5B")
+        self.sdata.penup(); self.sdata.speed(0); self.sdata.goto(0, 330); self.sdata.hideturtle()
+        self.sdata.color("white"); self.sdata.write("Score: 0 \t\tRecord:0", align="center", font=('Arial', 30, "bold"))
+        self.snake.shape("square"); self.snake.penup()
+        self.apples.shape("circle"); self.apples.penup(); self.apples.speed(0)
+        self.screen.tracer(0)
