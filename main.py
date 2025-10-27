ebb3e556fc85384da9f9cc641daf18a4b7a63016
Omar Reyes <150773197+OmarRL05@users.noreply.github.com>
2025-10-26 23:36:01 -0600

    feat: Implement apple respawn and scoring system (#2)
    * feat: Add apples collection system
    
    Spawns apple items randomly after the snake eats it.
    
    - Checks for apple collisions after each movement.
    - Updates the screen to render the score changes.
    
    * feat: Add snake's body increase
    
    Grows snake's body when eating an apple.
    
    - Creates new snake's body part.
    - Body tracks and follow the snake's head position.

--- main.py
+++ main.py
@@ -10,24 +10,30 @@ class Snake:
         screen (turtle.Screen): The main game window.
         snake (turtle.Turtle): The turtle object for the snake's head.
         sdata (turtle.Turtle): The turtle object for the scoreboard.
-        apples (turtle.Turtle): The turtle object for the food.
+        apple (turtle.Turtle): The turtle object for the food.
         delay (float): The refresh delay for the game loop.
         end_game (bool): Flag to indicate if the game is over.
+        apple_points (int): The apples score of the game.
+        snake_size (int): Indicate the snake size on the game.
+        snake_body (list): Contains turtle objects for the snake's body.
     """
     def __init__(self):
         """Initializes all the core components required to start a game session."""
         self.screen = turtle.Screen()
         self.snake = turtle.Turtle()
         self.sdata = turtle.Turtle()
-        self.apples = turtle.Turtle()
+        self.apple = turtle.Turtle()
         self.delay = 0.1
         self.end_game = False
+        self.apple_points, self.snake_size = 0, 0
+        self.snake_body = []
 
         self.screen.setup(width=800, height=800); self.screen.title("Snake Game"); self.screen.bgcolor("#329F5B")
         self.sdata.penup(); self.sdata.speed(0); self.sdata.goto(0, 330); self.sdata.hideturtle()
-        self.sdata.color("white"); self.sdata.write("Score: 0 \t\tRecord:0", align="center", font=('Arial', 30, "bold"))
+        self.sdata.color("white"); self.sdata.write(f"Score: {self.apple_points} \tSize: {self.snake_size}", align="center", font=('Arial', 30, "bold"))
         self.snake.shape("square"); self.snake.penup()
-        self.apples.shape("circle"); self.apples.penup(); self.apples.speed(0)
+        self.apple.shape("circle"); self.apple.penup(); self.apple.speed(0); self.apple.color("red")
+        self.apple.goto(random.randint(-390, 390), random.randint(-390, 390))
         self.screen.tracer(0)
 
     #Snake angles direction functions
@@ -57,20 +63,63 @@ class Snake:
         self.screen.onkeypress(self.snake_direction_left , "a")
         self.screen.onkeypress(self.snake_direction_right , "d")
 
+    def snake_body_move(self):
+        """Tracks snake's head position and moves its body along its direction"""
+        if self.snake_body != []:
+            for i in range(len(self.snake_body) - 1, 0, -1):
+                self.snake_body[i].goto(self.snake_body[i - 1].pos())
+            self.snake_body[0].goto(self.snake.position())
+
+    def snake_grow(self):
+        """Creates and adds a new segment to the snake's body.
+
+            Each new segment is initialized as a Turtle object, styled, and appended
+            to the `snake_body` list.
+        """
+        snake_chest = turtle.Turtle()
+        snake_chest.shape("square")
+        snake_chest.penup()
+        snake_chest.color("green")
+        snake_chest.speed(0)
+        snake_chest.goto(410, 410)
+        self.snake_body.append(snake_chest)
+
     def border_collision(self):
         """If Snake collides, modifies the end_game value to "True" to end the game."""
         if self.snake.xcor() < -390 or self.snake.xcor() > 390 or self.snake.ycor() < -390 or self.snake.ycor() > 390:
             time.sleep(self.delay)
             self.snake.goto(0,0)
             self.sdata.clear(); self.sdata.goto(0, 50)
-            self.sdata.color("red"); self.sdata.write("YOU LOSE\nScore: 0\t\tRecord: 0", align="center", font=('Arial', 30, "bold"));
+            self.sdata.color("red");
+            self.sdata.write(f"YOU LOSE\nScore: {self.apple_points}\tSize: {self.snake_size}", align="center", font=('Arial', 30, "bold"))
+            for chest in self.snake_body:
+                chest.hideturtle()
             self.end_game = True
+
+    def apple_collision(self):
+        """Checks for collision with the apple and updates the game state.
+
+            If the snake's head is close enough to the apple, this method
+            triggers the following events:
+            - The apple is moved to a new random location.
+            - The player's score and snake's size are incremented.
+            - The on-screen scoreboard is updated to reflect the new values.
+            """
+        if self.snake.distance(self.apple) < 20:
+            self.snake_grow()
+            self.apple_points += 10
+            self.snake_size += 1
+            self.apple.goto(random.randint(-390, 390), random.randint(-390, 390))
+            self.sdata.clear()
+            self.sdata.write(f"Score: {self.apple_points} \tSize: {self.snake_size}", align="center", font=('Arial', 30, "bold"))
 
     def start_snake(self):
         """Starts user's gameplay and updates screen information """
         while not self.end_game:
+            self.snake_body_move()
             self.snake.forward(20)
             self.border_collision()
+            self.apple_collision()
             self.screen.update()
             time.sleep(self.delay)
         self.screen.mainloop()
