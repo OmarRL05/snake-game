4283ac872e1b861fab59f1f4984a6934f9edd536
Omar Reyes L. <relo2210@gmail.com>
2025-10-26 23:11:09 -0600

    feat: Add snake's body increase
    Grows snake's body when eating an apple.
    
    - Creates new snake's body part.
    - Body tracks and follow the snake's head position.

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ class Snake:
         screen (turtle.Screen): The main game window.
         snake (turtle.Turtle): The turtle object for the snake's head.
         sdata (turtle.Turtle): The turtle object for the scoreboard.
-        apples (turtle.Turtle): The turtle object for the food.
+        apple (turtle.Turtle): The turtle object for the food.
         delay (float): The refresh delay for the game loop.
         end_game (bool): Flag to indicate if the game is over.
-        apple_points (int): The apples score of the game
-        snake_size (int): Indicate the snake size on the game
+        apple_points (int): The apples score of the game.
+        snake_size (int): Indicate the snake size on the game.
+        snake_body (list): Contains turtle objects for the snake's body.
     """
     def __init__(self):
         """Initializes all the core components required to start a game session."""
@@ -25,6 +26,7 @@ class Snake:
         self.delay = 0.1
         self.end_game = False
         self.apple_points, self.snake_size = 0, 0
+        self.snake_body = []
 
         self.screen.setup(width=800, height=800); self.screen.title("Snake Game"); self.screen.bgcolor("#329F5B")
         self.sdata.penup(); self.sdata.speed(0); self.sdata.goto(0, 330); self.sdata.hideturtle()
@@ -61,13 +63,37 @@ class Snake:
         self.screen.onkeypress(self.snake_direction_left , "a")
         self.screen.onkeypress(self.snake_direction_right , "d")
 
+    def snake_body_move(self):
+        """Tracks snake's head position and moves its body along its direction"""
+        if self.snake_body != []:
+            for i in range(len(self.snake_body) - 1, 0, -1):
+                self.snake_body[i].goto(self.snake_body[i - 1].pos())
+            self.snake_body[0].goto(self.snake.position())
+
+    def snake_grow(self):
+        """Creates and adds a new segment to the snake's body.
+
+            Each new segment is initialized as a Turtle object, styled, and appended
+            to the `snake_body` list.
+        """
+        snake_chest = turtle.Turtle()
+        snake_chest.shape("square")
+        snake_chest.penup()
+        snake_chest.color("green")
+        snake_chest.speed(0)
+        snake_chest.goto(410, 410)
+        self.snake_body.append(snake_chest)
+
     def border_collision(self):
         """If Snake collides, modifies the end_game value to "True" to end the game."""
         if self.snake.xcor() < -390 or self.snake.xcor() > 390 or self.snake.ycor() < -390 or self.snake.ycor() > 390:
             time.sleep(self.delay)
             self.snake.goto(0,0)
             self.sdata.clear(); self.sdata.goto(0, 50)
-            self.sdata.color("red"); self.sdata.write("YOU LOSE\nScore: 0\t\tRecord: 0", align="center", font=('Arial', 30, "bold"));
+            self.sdata.color("red");
+            self.sdata.write(f"YOU LOSE\nScore: {self.apple_points}\tSize: {self.snake_size}", align="center", font=('Arial', 30, "bold"))
+            for chest in self.snake_body:
+                chest.hideturtle()
             self.end_game = True
 
     def apple_collision(self):
@@ -80,17 +106,17 @@ class Snake:
             - The on-screen scoreboard is updated to reflect the new values.
             """
         if self.snake.distance(self.apple) < 20:
+            self.snake_grow()
             self.apple_points += 10
             self.snake_size += 1
             self.apple.goto(random.randint(-390, 390), random.randint(-390, 390))
-            self.sdata.clear();
+            self.sdata.clear()
             self.sdata.write(f"Score: {self.apple_points} \tSize: {self.snake_size}", align="center", font=('Arial', 30, "bold"))
-            self.screen.update()
-            time.sleep(self.delay)
 
     def start_snake(self):
         """Starts user's gameplay and updates screen information """
         while not self.end_game:
+            self.snake_body_move()
             self.snake.forward(20)
             self.border_collision()
             self.apple_collision()
