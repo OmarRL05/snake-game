eff07faf15997edb94ecee3db99f95cd15e76132
Omar Reyes <150773197+OmarRL05@users.noreply.github.com>
2025-10-28 00:12:40 -0600

    feat: Add restart system (#6)
    * refactor: Extract lose screen logic into a function
    
    Restructured lose screen as loose_screen function.
    
    * feat: Add restart system after losing
    
    Once user loses can restart the game by pressing `space`.
    
    - Start indicator text box.
    - Restart snake's size.
    - Now listener is not required on snake_move function
    
    * fix: Fix restart score system after losing error
    
    The score couldn't restart when `space` was pressed.
    
    - Restart score now init by 0
    - Restart function added

--- main.py
+++ main.py
@@ -19,18 +19,21 @@ class Snake:
     """
     def __init__(self):
         """Initializes all the core components required to start a game session."""
-        self.screen = turtle.Screen()
+        self.screen = turtle.Screen(); self.screen.listen() #Listener activation
         self.snake = turtle.Turtle()
         self.sdata = turtle.Turtle()
+        self.lose = turtle.Turtle()
         self.apple = turtle.Turtle()
         self.delay = 0.1
-        self.end_game = False
         self.apple_points, self.snake_size = 0, 0
+        self.end_game = False
+        self.press_x = False
         self.snake_body = []
 
         self.screen.setup(width=800, height=800); self.screen.title("Snake Game"); self.screen.bgcolor("#329F5B")
         self.sdata.penup(); self.sdata.speed(0); self.sdata.goto(0, 330); self.sdata.hideturtle()
         self.sdata.color("white"); self.sdata.write(f"Score: {self.apple_points} \tSize: {self.snake_size}", align="center", font=('Arial', 30, "bold"))
+        self.lose.hideturtle(); self.lose.penup(); self.lose.color("red"); self.lose.speed(0); self.lose.goto(0, 20)
         self.snake.shape("square"); self.snake.penup()
         self.apple.shape("circle"); self.apple.penup(); self.apple.speed(0); self.apple.color("red")
         self.apple.goto(random.randint(-390, 390), random.randint(-390, 390))
@@ -57,7 +60,6 @@ class Snake:
             Sets up the game to listen for key presses and maps the WASD Keys to the corresponding snake
             direction methods.
         """
-        self.screen.listen() #Listener activation
         self.screen.onkeypress(self.snake_direction_up, "w")
         self.screen.onkeypress(self.snake_direction_down , "s")
         self.screen.onkeypress(self.snake_direction_left , "a")
@@ -93,20 +95,12 @@ class Snake:
         """
         for chest in self.snake_body[1:]:
             if self.snake.distance(chest) < 10:
-                self.end_game = True
+                self.lose_screen()
 
     def border_collision(self):
         """If Snake collides, modifies the end_game value to "True" to end the game."""
         if self.snake.xcor() < -390 or self.snake.xcor() > 390 or self.snake.ycor() < -390 or self.snake.ycor() > 390:
-            time.sleep(self.delay)
-            self.snake.goto(0,0)
-            self.sdata.clear(); self.sdata.goto(0, 50)
-            self.sdata.color("red")
-            self.sdata.write(f"YOU LOSE\nScore: {self.apple_points}\tSize: {self.snake_size}", align="center", font=('Arial', 30, "bold"))
-            for chest in self.snake_body:
-                chest.hideturtle()
-            self.snake_body.clear()
-            self.end_game = True
+            self.lose_screen()
 
     def apple_collision(self):
         """Checks for collision with the apple and updates the game state.
@@ -122,12 +116,34 @@ class Snake:
             self.apple_points += 10
             self.snake_size += 1
             self.apple.goto(random.randint(-390, 390), random.randint(-390, 390))
-            self.sdata.clear()
-            self.sdata.write(f"Score: {self.apple_points} \tSize: {self.snake_size}", align="center", font=('Arial', 30, "bold"))
+            self.score_screen()
+
+    def score_screen(self):
+        self.sdata.clear()
+        self.sdata.write(f"Score: {self.apple_points} \tSize: {self.snake_size}", align="center", font=('Arial', 30, "bold"))
+
+    def lose_screen(self):
+        time.sleep(self.delay)
+        self.lose.write("\tYou Lose\n Press Space to Play again", align="center", font=('Arial', 40, "bold"))
+        self.end_game = True
+        self.screen.update()
+
+    def _restart(self):
+        self.snake.goto(0, 0)
+        for chest in self.snake_body:
+            chest.hideturtle()
+        self.snake_body.clear()
+        self.apple_points, self.snake_size = 0, 0
+        self.score_screen()
+        self.apple.goto(random.randint(-390, 390), random.randint(-390, 390))
+        self.start_snake()
 
     def start_snake(self):
         """Starts user's gameplay and updates screen information """
+        time.sleep(1)
         self.snake_move()
+        self.lose.clear()
+        self.screen.update() #Clear screen before start playing
         while not self.end_game:
             self.snake_body_move()
             self.snake.forward(20)
@@ -135,11 +151,33 @@ class Snake:
             self.apple_collision()
             self.screen.update()
             time.sleep(self.delay)
-
-        self.apple_points, self.snake_size = 0, 0
         self.end_game = False
 
+    def menu_keys(self):
+        self.screen.onkeypress(self._restart, "space")
+
+    def on_game(self):
+
+        """Main function
+
+            Allows to restart once a game ends. Restarts the snake size, respawns the apple again and cleans the
+            full snake's body.
+        """
+        #Welcome screen
+        welcome_text = turtle.Turtle()
+        welcome_text.hideturtle()
+        welcome_text.penup()
+        welcome_text.sety(-50)
+        welcome_text.color("white")
+        welcome_text.write("Press 'space' to start", align="center", font=('Arial', 25, "normal"))
+        self.screen.update()
+        time.sleep(3)
+        welcome_text.clear()
+        self.screen.update()
+        #Reading `Space` to play again
+        self.menu_keys()
+        #Screen updates ending
         self.screen.mainloop()
 
 Snake_game = Snake()
-Snake_game.start_snake()
+Snake_game.on_game()
